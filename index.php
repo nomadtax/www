bf1683af7bb36ea5e79ec4be360d61377d55ea80
Rick Graham <dynokicks@gmail.com>
2019-06-17 11:56:59 -0400

    spacing adjustments for desktop

--- index.php
+++ index.php
@@ -135,7 +135,7 @@
         <!-- "dark": dark version of header e.g. class="header dark clearfix" -->
         <!-- "centered": mandatory class for the centered logo layout -->
         <!-- ================ -->
-        <header class="header dark fixed fixed-desktop clearfix ">
+        <header class="header dark fixed fixed-all clearfix ">
           <div class="container">
             <div class="row">
               <div class="col-md-auto hidden-md-down">
@@ -145,7 +145,7 @@
 
                   <!-- logo -->
                   <div id="logo" class="logo">
-                    <a href="/"><img id="logo_img" src="nomadtax/nt_full_logo_light.png" alt="Nomad Tax" class="nt-logo" style="height:55px;"></a>
+                    <a href="/"><img id="logo_img" src="nomadtax/nt_full_logo_light.png" alt="Nomad Tax" class="nt-logo"></a>
                   </div>
 
                   <!-- name-and-slogan -->
@@ -168,7 +168,7 @@
                 <!-- "onclick": Makes the dropdowns open on click, this the default bootstrap behavior e.g. class="main-navigation onclick" -->
                 <!-- "animated": Enables animations on dropdowns opening e.g. class="main-navigation animated" -->
                 <!-- ================ -->
-                <div class="main-navigation main-navigation--mega-menu main-navigation--style-2  animated">
+                <div class="main-navigation main-navigation--style-2  animated">
                   <nav class="navbar navbar-expand-lg navbar-light p-0">
                     <div class="navbar-brand clearfix hidden-lg-up">
 
@@ -183,24 +183,6 @@
                       </div>
 
                     </div>
-
-                    <!-- header dropdown buttons -->
-                    <div class="header-dropdown-buttons hidden-lg-up p-0 ml-auto mr-3">
-                      <div class="btn-group">
-                        <button type="button" class="btn dropdown-toggle dropdown-toggle--no-caret" id="header-drop-3" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-search"></i></button>
-                        <ul class="dropdown-menu dropdown-menu-right dropdown-animation" aria-labelledby="header-drop-3">
-                          <li>
-                            <form role="search" class="search-box margin-clear">
-                              <div class="form-group has-feedback">
-                                <input type="text" class="form-control" placeholder="Search">
-                                <i class="fa fa-search form-control-feedback"></i>
-                              </div>
-                            </form>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                    <!-- header dropdown buttons end -->
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
@@ -278,8 +260,8 @@
                 <div class="background-image-overlay"></div>
                 <div class="container">
                   <div class="hc-slideshow-caption hc-slideshow-caption--large p-3 text-center w-100">
-                    <img src="nomadtax/nt_seal_light.png" alt="Nomad Tax" style="height:180px;display:inline-block;" class="text-white text-capitalize animated fadeInDown hc-animation-delay-1000"/>
-                    <h1 class="text-white text-capitalize animated fadeInDown hc-animation-delay-1250">Nomad Tax</h1>
+                    <img src="nomadtax/nt_seal_light.png" alt="Nomad Tax" style="height:180px;display:inline-block;" class="text-white text-capitalize animated fadeInDown hc-animation-delay-1250"/>
+                    <h1 class="text-white text-capitalize animated fadeInDown hc-animation-delay-750">Nomad Tax</h1>
                     <p class="animated fadeInUp hc-animation-delay-1500">
                       Guiding digital nomads through tax landscapes.
                       <br class="d-none d-sm-inline">
@@ -349,13 +331,14 @@
 
       <!-- section start -->
       <!-- ================ -->
-      <section class="pv-20">
+      <section class="pv-20" id="why">
+        &nbsp;
       </section>
       <!-- section end -->
 
       <!-- section -->
       <!-- ================ -->
-      <section class="pv-30" id="why">
+      <section class="pv-30">
         <div class="container">
           <div class="row justify-content-md-center">
             <div class="col-lg-8">
@@ -604,9 +587,16 @@
       </section>
       <!-- section end -->
 
+      <!-- section start -->
+      <!-- ================ -->
+      <section class="pv-10" id="about">
+        &nbsp;
+      </section>
+      <!-- section end -->
+
       <!-- main-container start -->
       <!-- ================ -->
-      <section id="about" class="main-container padding-bottom-clear" id="about">
+      <section id="about" class="main-container padding-bottom-clear">
         <div class="container">
           <div class="row">
             <!-- main start -->
@@ -653,15 +643,15 @@
 
       <!-- section start -->
       <!-- ================ -->
-      <section class="pv-20">
+      <section class="pv-40">
       </section>
       <!-- section end -->
 
 
       <!-- footer start (Add "dark" class to #footer in order to enable dark footer) -->
       <!-- ================ -->
-      <footer id="footer" class="clearfix dark" name="contact">
-
+      <footer id="footer" class="clearfix dark">
+        <br />
         <!-- .footer start -->
         <!-- ================ -->
         <div class="footer text-white">
@@ -669,7 +659,9 @@
             <div class="footer-inner">
               <div class="row">
                 <div class="col-12">
+                  <br />
                   <div id="contact" class="footer-content w-form">
+                    <br />
                     <h2 class="display-4 text-center font-weight-bold">Contact Us</h2>
                     <form action="https://nomadtax.us7.list-manage.com/subscribe/post?u=1dc80cd4d54d6b9199b6cc4ec&id=59f64e822b" method="post" class="mt-3 form-style-2 text-center">
                       <div class="form-group has-feedback mb-10">
@@ -696,6 +688,7 @@
                       <input type="submit" value="Send Message" class="mt-3 submit-button radius-50 btn btn-outline-light btn-lg">
 
                     </form>
+                    <br />
                     <div class="success-message-4 w-form-done">
                       <div class="text-block-8">Thank you! Your submission has been received!</div>
                     </div>
