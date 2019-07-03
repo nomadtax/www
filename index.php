ccef0841f19176e41006fe7a96bccbf83da58cda
Rick Graham <dynokicks@gmail.com>
2019-07-03 21:34:10 +0200

    fix medium url

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
                     <li class="linkedin"><a href="https://www.linkedin.com/company/nomad-tax-pllc/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
                     <li class="instagram"><a href="https://www.instagram.com/nomadtax/" target="_blank"><i class="fa fa-instagram"></i></a></li>
                     <li class="facebook"><a href="https://www.facebook.com/nomadtax/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                    <li class="medium"><a href="https://medium.com/nomad-tax" target="_blank"><i class="fa fa-medium"></i></a></li>
+                    <li class="medium"><a href="https://medium.com/@nomadtax" target="_blank"><i class="fa fa-medium"></i></a></li>
                   </ul>
                   <div class="social-links hidden-md-up circle small">
                     <div class="btn-group dropdown">
@@ -90,7 +90,7 @@
                         <li class="linkedin"><a href="https://www.linkedin.com/company/nomad-tax-pllc/" target="_new"><i class="fa fa-linkedin"></i></a></li>
                         <li class="instagram"><a href="https://www.instagram.com/nomadtax/" target="_new"><i class="fa fa-instagram"></i></a></li>
                         <li class="facebook"><a href="https://www.facebook.com/nomadtax/" target="_new"><i class="fa fa-facebook"></i></a></li>
-                        <li class="medium"><a href="https://medium.com/nomad-tax" target="_new"><i class="fa fa-medium"></i></a></li>
+                        <li class="medium"><a href="https://medium.com/@nomadtax" target="_new"><i class="fa fa-medium"></i></a></li>
                       </ul>
                     </div>
                   </div>
@@ -700,7 +700,7 @@
                       <li><a href="https://twitter.com/nomadtax"><i class="fa fa-twitter"></i></a></li>
                       <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                       <li><a href="https://www.linkedin.com/company/nomad-tax-pllc/"><i class="fa fa-linkedin"></i></a></li>
-                      <li><a href="https://medium.com/nomad-tax"><i class="fa fa-medium"></i></a></li>
+                      <li><a href="https://medium.com/@nomadtax"><i class="fa fa-medium"></i></a></li>
                     </ul>
                     <p class="text-center text-muted">Copyright © Nomad Tax 2018. All Rights Reserved</p>
                   </div>
